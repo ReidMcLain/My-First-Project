488a049d47f007bb26047c687ed69a8e8b89c0aa
Reid McLain <97499183+ReidMcLain@users.noreply.github.com>
2022-09-03 08:49:58 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function GetValue()
+{
+    var myarray= new Array("item1","item2","item3");
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+    //alert(random);
+    document.getElementById("message").innerHTML=random;
+}
