19bccf345cdd9e5ce68df832343a476068facc71
Reid McLain <97499183+ReidMcLain@users.noreply.github.com>
2022-09-06 10:33:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ function GetValue()
     var random = myarray[Math.floor(Math.random() * myarray.length)];
     //alert(random);
     document.getElementById("message").innerHTML=random;
-}
-
-function randomIntFromInterval(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-const rndInt = randomIntFromInterval(1, 6)
-console.log(rndInt)
+};
 
 function reidSmile()
 {
 var smile =":)"
 document.getElementById("message1").innerHTML = smile;
-}
+};
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
+function guessYourWeight()
+{
+const rndInt = randomIntFromInterval(100, 300)
+document.getElementById("message2").innerHTML = rndInt;
+};
